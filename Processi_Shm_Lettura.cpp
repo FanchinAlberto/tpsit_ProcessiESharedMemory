41d63a70130594cd3a76eec222f3fc38c54a11bf
FanchinAlberto <83753418+FanchinAlberto@users.noreply.github.com>
2022-03-31 09:19:46 +0200

    Update Processi_Shm_Lettura.cpp

--- Processi_Shm_Lettura.cpp
+++ Processi_Shm_Lettura.cpp
@@ -38,12 +38,27 @@ int main()
     int shmid = shmget(key, sizeof(filename), 0666|IPC_CREAT); //richiedo al siatema un blocco di memoria 
     string *name = (string*)shmat(shmid, NULL, 0); //prendo il blocco di memoria condiviso per utilizzarlo
     *name = filename;
+     string poesia = "Onde dorate, e l’onde eran capelli,\n"
+     "navicella d’avorio un dì fendea;\n"
+     "una man pur d’avorio la reggea\n"
+     "per questi errori preziosi e quelli;\n"
+     "\n"
+     "e, mentre i flutti tremolanti e belli\n"
+     "con drittissimo solco dividea,\n"
+     "l’òr delle rotte fila Amor cogliea,\n"
+     "per formarne catene a’ suoi rubelli.\n"
+     "\n"
+     "Per l’aureo mar, che rincrespando apria\n"
+     "il procelloso suo biondo tesoro,\n"
+     "agitato il mio core a morte gìa.\n"
+     "\n"
+     "Ricco naufragio, in cui sommerso io moro,\n"
+     "poich’almen fur, ne la tempesta mia,\n"
+     "di diamante lo scoglio e ‘l golfo d’oro!"
     ofstream myfile (filename);
         if (myfile.is_open()) //scrivo il file
         {
-            myfile << "Fanchin Alberto\n";
-            myfile << "4CII 31/03/2022\n";
-            myfile << "Esercizio Processi, Shared Memory e Lettura + Scrittura di file\n";
+            myfile << poesia;
             myfile.close();
         }
     fork_getFileName(name);
